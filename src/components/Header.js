9859d447539e483ed88e5fb4af7463614ef9cf37
MThos <MThos@users.noreply.github.com>
2023-03-28 09:22:11 -0400

    fixed header theming

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,10 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { TextField } from "@mui/material";
 import Autocomplete from '@mui/material/Autocomplete';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from "@emotion/react";
 
-const Navbar = () => {
+const Header = () => {
   // nav links
   const [anchorNavEl, setAnchorNavEl] = React.useState(null);
   const openNav = Boolean(anchorNavEl);
@@ -52,7 +54,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="font-custom bg-dark-purple-700 text-light-purple-500 flex flex-row items-center justify-between">
+    <div className="bg-dark-purple-700 text-light-purple-500 flex flex-row items-center justify-between">
       <img className="w-16 ml-2" src="images/lovethestocks-min.png" alt="logo" />
 
       <Button
@@ -67,52 +69,34 @@ const Navbar = () => {
       >
         <MenuIcon className="cursor-pointer text-light-purple-500" />
       </Button>
-      <Menu
-        id="nav-menu"
-        anchorEl={anchorNavEl}
-        open={openNav}
-        onClose={handleNavClose}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-        sx={{
-          '& .MuiMenu-paper': {
-            border: '2px solid black',
-            backgroundColor: '#493e8e',
-            color: 'white',
-            textTransform: 'uppercase'
-          },
-          '& .MuiMenuItem-root': {
-            border: '1px solid black',
-            fontSize: 14,
-            py: 0,
-            px: 5 
-          },
-          '& .MuiMenuItem-root:hover': {
-            backgroundColor: 'white',
-            color: 'black'
-          },
-          '& .MuiList-root': {
-            padding: 0
-          },
-        }}
-      >
-        <MenuItem onClick={handleNavClose}>Latest News</MenuItem>
-        <MenuItem onClick={handleNavClose}>Index List</MenuItem>
-        <MenuItem onClick={handleNavClose}>Charts</MenuItem>
-        <MenuItem onClick={handleNavClose}>Details</MenuItem>
-        <MenuItem onClick={handleNavClose}>Financials</MenuItem>
-        <MenuItem onClick={handleNavClose}>Analysis</MenuItem>
-        <MenuItem onClick={handleNavClose}>Company</MenuItem>
-      </Menu>
+      
+      <ThemeProvider theme={navMenuTheme}>
+        <Menu
+          id="nav-menu"
+          anchorEl={anchorNavEl}
+          open={openNav}
+          onClose={handleNavClose}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          MenuListProps={{
+            'aria-labelledby': 'nav-menu',
+          }}
+        >
+          <MenuItem onClick={handleNavClose}>Latest News</MenuItem>
+          <MenuItem onClick={handleNavClose}>Index List</MenuItem>
+          <MenuItem onClick={handleNavClose}>Charts</MenuItem>
+          <MenuItem onClick={handleNavClose}>Details</MenuItem>
+          <MenuItem onClick={handleNavClose}>Financials</MenuItem>
+          <MenuItem onClick={handleNavClose}>Analysis</MenuItem>
+          <MenuItem onClick={handleNavClose}>Company</MenuItem>
+        </Menu>
+      </ThemeProvider>
 
       <Button
         id="search-button"
@@ -120,91 +104,169 @@ const Navbar = () => {
         aria-haspopup="true"
         aria-expanded={openSearch ? 'true' : undefined}
         onClick={handleSearchClick}
-        sx={{
-          p: 0
-        }}
+        sx={{ p: 0 }}
       >
         <SearchIcon className="cursor-pointer text-light-purple-500" />
       </Button>
-      <Menu
-        id="search-menu"
-        anchorEl={anchorSearchEl}
-        open={openSearch}
-        onClose={handleSearchClose}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-        sx={{
-          '& .MuiMenu-paper': {
-            border: '2px solid black',
-            backgroundColor: '#493e8e',
-            color: 'white',
-            width: '100%'
-          },
-          '& .MuiMenuItem-root': {
-            border: 0,
-            py: 0,
-            px: 0,
-          },
-          '& .MuiMenuItem-root:hover': {
-            backgroundColor: '#493e8e',
-            color: 'white',
-            padding: 0
-          },
-          '& .MuiPaper-root': {
-            backgroundColor: '#493e8e'
-          },
-          '& .MuiList-root': {
-            padding: 0
-          },
-        }}
-      >
-        <MenuItem onKeyDown={e => handleKeyPress(e)}>
-          <Autocomplete 
-            {...symbols}
-            id="ticker-search"
-            autoHighlight
-            sx={{
-              width: '100%',
-              "& .MuiInputBase-input": {
-                textTransform: 'uppercase',
-              },
-              "& .MuiAutocomplete-inputRoot": {
-                color: "white",
-              },
-              "& .MuiButtonBase-root": {
-                color: 'white'
-              },
-              "& .MuiOutlinedInput-notchedOutline": {
-                borderColor: "black"
-              },
-              "& :hover .MuiOutlinedInput-notchedOutline": {
-                borderColor: "white",
-              },
-              "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-                borderColor: "white"
-              },
-            }}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label=""
-                placeholder="search a company"
+
+      <ThemeProvider theme={searchMenuTheme}>
+        <Menu
+          id="search-menu"
+          anchorEl={anchorSearchEl}
+          open={openSearch}
+          onClose={handleSearchClose}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <MenuItem onKeyDown={e => handleKeyPress(e)}>
+            <ThemeProvider theme={autocompleteMenuTheme}>
+              <Autocomplete 
+                {...symbols}
+                id="ticker-search"
+                autoHighlight
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label=""
+                    placeholder="search a company"
+                  />
+                )}
               />
-            )}
-          />
-        </MenuItem>
-      </Menu>
+            </ThemeProvider>
+          </MenuItem>
+        </Menu>
+      </ThemeProvider>
     </div>
   )
 }
 
-export default Navbar;
+export default Header;
+
+const navMenuTheme = createTheme({
+  typography: {
+    "fontFamily": `"Nanum Gothic Coding", Helvetica, sans-serif`,
+  },
+  components: {
+    MuiMenu: {
+      styleOverrides: {
+        paper: {
+          border: '2px solid black',
+          backgroundColor: '#493e8e',
+          color: 'white',
+          textTransform: 'uppercase'
+        }
+      },
+    },
+    MuiMenuItem: {
+      styleOverrides: {
+        root: {
+          border: '1px solid black',
+          fontSize: '0.9rem',
+          '&:hover': {
+            backgroundColor: 'white',
+            color: 'black'
+          }
+        }
+      }
+    },
+    MuiList: {
+      styleOverrides: {
+        root: {
+          padding: 0
+        }
+      }
+    }
+  }
+});
+
+const searchMenuTheme = createTheme({
+  typography: {
+    "fontFamily": `"Nanum Gothic Coding", Helvetica, sans-serif`,
+  },
+  components: {
+    MuiMenu: {
+      styleOverrides: {
+        paper: {
+          border: '2px solid black',
+          backgroundColor: '#241f47',
+          color: 'white',
+          width: '100%'
+        }
+      },
+    },
+    MuiMenuItem: {
+      styleOverrides: {
+        root: {
+          border: 0,
+          padding: 0,
+          '&:hover': {
+            backgroundColor: '#493e8e',
+            color: 'white',
+            padding: 0
+          }
+        }
+      }
+    },
+    MuiList: {
+      styleOverrides: {
+        root: {
+          padding: 0
+        }
+      }
+    },
+  }
+});
+
+const autocompleteMenuTheme = createTheme({
+  typography: {
+    "fontFamily": `"Nanum Gothic Coding", Helvetica, sans-serif`,
+  },
+  components: {
+    MuiInputBase: {
+      styleOverrides: {
+        input: {
+          color: 'white',
+          textTransform: 'uppercase'
+        },
+      }
+    },
+    MuiAutocomplete: {
+      styleOverrides: {
+        root: {
+          width: '100%'
+        },
+        option: {
+          backgroundColor: '#493e8e',
+          color: 'white',
+          '&:hover': {
+            backgroundColor: 'white',
+            color: 'black'
+          },
+          '&.Mui-focused': {
+            color: 'black'
+          }
+        },
+        listbox: {
+          fontSize: '1rem',
+          padding: 0
+        }
+      }
+    },
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          color: 'white'
+        }
+      }
+    }
+  }
+});
