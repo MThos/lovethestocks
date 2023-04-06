1702ac9a4c880aea6157a57c800791c0f28f49b0
MThos <MThos@users.noreply.github.com>
2023-04-06 07:43:22 -0400

    added onClick handler on news symbol click

--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import LoadingIcon from './LoadingIcon.js';
 import Card from '@mui/material/Card';
@@ -11,10 +12,6 @@ import Typography from '@mui/material/Typography';
 const News = () => {
   const [newsData, setNewsData] = useState([]);
   const [isLoading, setLoading] = useState(true);
-
-  const OnClickSymbolHandler = (symbol) => {
-    localStorage.setItem('active', symbol.toUpperCase());
-  };
 
   useEffect(() => {
     try {
@@ -45,7 +42,9 @@ const News = () => {
     <section className="fade-in">
       <div className="flex flex-col text-center font-custom font-semibold text-white">
         <div className="text-5xl font-bold mt-4">NEWS</div>
-        { NewsList(newsData) }
+        <div className="flex flex-row flex-wrap">
+          { NewsList(newsData) }
+        </div>
       </div>
     </section>
   )  
@@ -57,33 +56,40 @@ const NewsList = (newsData) => {
   const news = [];
   let counter = 0;
 
+  const OnClickSymbolHandler = (symbol) => {
+    localStorage.setItem('active', symbol.toUpperCase());
+  };
+
   newsData.map((key) => (
     news.push(
-      <Card key={counter++} className="m-auto mt-10" sx={{ border: '2px solid #8B5CF6', maxWidth: 350 }}>
-        <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image={`${key['image']}`}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div" color="text.main" sx={{ fontWeight: 700 }}>
-            {key['symbol']}
-          </Typography>
-          <Typography gutterBottom component="div" color="text.main">
+      <div className="grow shrink sm:basis-full md:basis-1/2 lg:basis-1/2 xl:basis-1/3">
+        <Card key={counter++} className="m-auto mt-10" sx={{ background: '#3709A1', border: '4px solid #fff', maxWidth: 350 }}>
+          <CardMedia
+          component="img"
+          alt={key['title']}
+          height="140"
+          image={`${key['image']}`}
+          />
+          <CardContent>
+            <Link to={'/company'}>
+              <Typography onClick={() => OnClickSymbolHandler(key['symbol']) } gutterBottom variant="h4" component="div" color="text.main" sx={{ fontWeight: 700 }}>
+                {key['symbol']}
+              </Typography>
+            </Link>
+            <Typography variant="body1" color="text.main" sx={{ fontWeight: 400, fontSize: 18, lineHeight: 1.5 }}>
+              {key['title']}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="large" sx={{ color: '#F6CFFC', fontSize: 18, fontWeight: 400, margin: 'auto' }}>
+              <a href={`${key['url']}`} target="_blank" rel="noopener noreferrer">Read Full Article</a>
+            </Button>
+          </CardActions>
+          <Typography gutterBottom component="div" color="text.main" sx={{ marginTop: 2 }}>
             {key['publishedDate']}
           </Typography>
-          <br/>
-          <Typography variant="body1" color="text.main" sx={{ fontWeight: 700, lineHeight: 1.5 }}>
-            {key['title']}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="large" sx={{ color: '#F6CFFC', fontSize: '1rem', fontWeight: 700, margin: 'auto' }}>
-            <a href={`${key['url']}`} target="_blank" rel="noopener noreferrer">Read Full Article</a>
-          </Button>
-        </CardActions>
-      </Card>
+        </Card>
+      </div>      
     )
   ))
 
